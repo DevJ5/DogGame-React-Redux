a92215ebab8067c89b1d6487e0e13e09913d5548
Vince Liem <vincepaul.liem@gmail.com>
2018-10-02 09:59:30 +0200

    small refactor in AppAction

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,42 +1,29 @@
 import {SET_BREEDS} from "../reducers/allBreeds";
 import {SET_CORRECT_BREED} from "../reducers/correctBreed";
-import capitalize from "../helpers/capitalize";
 import {GET_ANSWERS} from "../reducers/answers";
+
+import capitalize from "../helpers/capitalize";
 
 export const setBreeds = allBreeds => ({
   type: SET_BREEDS,
   payload: Object.keys(allBreeds)
 });
 
-export const setCorrectBreed = CorrectBreed => {
-
-  const linkToName = link => {
-    const name = link.split('/')[4];
-    if (name.includes('-')) return capitalize(name.split('-')[0]);
-    return capitalize(name);
-  };
-
-  return {
-    type: SET_CORRECT_BREED,
-    payload: {
-      image: CorrectBreed,
-      name: linkToName(CorrectBreed)
-    }
+export const setCorrectBreed = CorrectBreed => ({
+  type: SET_CORRECT_BREED,
+  payload: {
+    image: CorrectBreed,
+    name: CorrectBreed.split('/')[4].split('-')[0]
   }
-
-};
+});
 
 export const getAnswers = (correctBreed, allBreeds) => {
 
-  const getRandomBreed = allBreeds => capitalize(allBreeds[Math.floor(Math.random() * allBreeds.length)]);
-
-  const randomBreeds = allBreeds => {
-    return [getRandomBreed(allBreeds), getRandomBreed(allBreeds), correctBreed]
-  };
+  const getRandomBreed = () => allBreeds[Math.floor(Math.random() * allBreeds.length)];
 
   return ({
     type: GET_ANSWERS,
-    payload: randomBreeds(allBreeds)
+    payload: [getRandomBreed(), getRandomBreed(), correctBreed].map(breed => capitalize(breed))
   })
 };
 
