baa90d81e6d533923cdf26c7834c727ed414e46a
Vince Liem <vincepaul.liem@gmail.com>
2018-10-02 15:39:43 +0200

    Moved component of MAIN and followed Jeroens instructions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Image} from "./Image";
+import {Buttons} from "./Buttons";
+import * as PropTypes from "prop-types";
 
-export default class Main extends Component {
+export class Main extends Component {
   render() {
-    return <main>test text.</main>;
+    return (
+      <div>{this.props.correctBreed && (
+        <Image correctBreed={this.props.correctBreed}/>
+      )}
+        {this.props.answers.length > 0 && (
+          <Buttons answers={this.props.answers} onClick={this.props.onClick}/>
+        )}</div>)
+      ;
   }
 }
+
+Main.propTypes = {
+  correctBreed: PropTypes.any,
+  answers: PropTypes.any,
+  onClick: PropTypes.func
+};
