7346e7e862fbd470b1bc97d471d8421715918bb8
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-10-02 14:07:58 +0200

    correct answers given counter working

--- src/functions/userFeedback.js
+++ src/functions/userFeedback.js
@@ -1,30 +1,32 @@
-export default (targetValue, correct, nextQuestion) => {
+export default (
+  targetValue,
+  correct,
+  nextQuestion,
+  incrementQuestionsAsked
+) => {
   const bodyStyle = document.body.style;
 
   if (targetValue === correct) {
     // Show something green
-
+    incrementQuestionsAsked(); // dispatch the ADD_TO_SCORE
     bodyStyle.backgroundColor = 'green';
 
     setTimeout(() => {
-
       bodyStyle.backgroundColor = 'white';
 
       nextQuestion();
     }, 500);
-
   } else {
     // Show something red and wait 2 seconds before showing
 
     const correctButton = document.getElementById('button-' + correct);
     const wrongButton = document.getElementById('button-' + targetValue);
 
-      bodyStyle.backgroundColor = 'red';
-      correctButton.className = 'correct-button';
-      wrongButton.className = 'wrong-button';
+    bodyStyle.backgroundColor = 'red';
+    correctButton.className = 'correct-button';
+    wrongButton.className = 'wrong-button';
 
     setTimeout(() => {
-
       bodyStyle.backgroundColor = 'white';
       correctButton.className = 'default-button';
       wrongButton.className = 'default-button';
@@ -32,6 +34,4 @@ export default (targetValue, correct, nextQuestion) => {
       nextQuestion();
     }, 2000);
   }
-
-
-}
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,11 @@ import { combineReducers } from 'redux';
 import allBreeds from './allBreeds';
 import correctBreed from './correctBreed';
 import answers from './answers';
-import displayedBreeds from './displayedBreeds';
+import scoreChecker from './scoreChecker';
 
 export default combineReducers({
   allBreeds,
   correctBreed,
-  answers
-  // displayedBreeds
+  answers,
+  scoreChecker
 });
