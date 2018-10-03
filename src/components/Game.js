04e5b88b3071d0cc2a044298c99842f5f56fb81b
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-10-03 11:37:08 +0200

    Refactored Buttons and ButtonsContainer

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,23 @@
-import ScoreContainer from "../containers/ScoreContainer";
-import {Main} from "./Main";
-import Streak from "./scores/Streak";
-import * as PropTypes from "prop-types";
-import React from "react";
+import ScoreContainer from '../containers/ScoreContainer';
+import { Main } from './Main';
+import Streak from './scores/Streak';
+import * as PropTypes from 'prop-types';
+import React from 'react';
 
-import '../styles/scores.css'
+import '../styles/scores.css';
 
 export function Game(props) {
-  return <div className={"Game"}>
-    <ScoreContainer />
-    <Main
-      correctBreed={props.correctBreed}
-      answers={props.answers}
-      onClick={props.onClick}
-    />
-    <Streak streak={props.streak}/>
-  </div>;
+  return (
+    <div className={'Game'}>
+      <ScoreContainer />
+      <Main
+        correctBreed={props.correctBreed}
+        answers={props.answers}
+        onClick={props.onClick}
+      />
+      <Streak streak={props.streak} />
+    </div>
+  );
 }
 
 Game.propTypes = {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
-import {Image} from "./Image";
-import {Buttons} from "./Buttons";
-import * as PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { Image } from './Image';
+import * as PropTypes from 'prop-types';
 
-import '../styles/main.css'
+import '../styles/main.css';
+import ButtonsContainer from '../containers/ButtonsContainer';
 
 export class Main extends Component {
   render() {
     return (
       <div className={'Main'}>
         {this.props.correctBreed && (
-          <Image correctBreed={this.props.correctBreed}/>
+          <Image correctBreed={this.props.correctBreed} />
         )}
         {this.props.answers.length > 0 && (
-          <Buttons answers={this.props.answers} onClick={this.props.onClick}/>
+          <ButtonsContainer onClick={this.props.onClick} />
         )}
       </div>
     );
